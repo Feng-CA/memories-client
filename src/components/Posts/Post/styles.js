22bd3817785d2d2001a13cefdb579489edef3f0d
Feng-CA <gcam022106@coderacademy.edu.au>
2022-08-13 00:04:46 +1000

    creates post

--- src/components/Posts/Post/styles.js
+++ src/components/Posts/Post/styles.js
@@ -24,13 +24,13 @@ export default makeStyles({
     overlay: {
         position: 'absolute',
         top: '20px',
-        left: '2opx',
+        left: '20px',
         color: 'white',
     },
     overlay2: {
         position: 'absolute',
         top: '20px',
-        left: '2opx',
+        right: '20px',
         color: 'white',
     },
     grid: {
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
+import { useSelector } from 'react-redux';
+
 import Post from './Post/Post';
+import useStyles from './styles';
 
 const Posts = () => {
-  return (
-    <>
-        <h1>POSTS</h1>
-        <Post />
-        <Post />
-    </>
-  );
+    const posts = useSelector((state) => state.posts);
+    const classes = useStyles();
+
+	console.log(posts);
+
+	return (
+		!posts.length ? <CircularProgress /> : (
+			<Grid className={classes.container} container alignItems='stretch' spacing={3}>
+				{posts.map((post) => (
+					<Grid key={post.id} item xs={12} sm={6}>
+						<Post post={post} />
+					</Grid>
+				))}
+			</Grid>
+		)
+	)
 }
 
 export default Posts;
